87a2efad29be9f32c4752c76943f4e574b513a54
neerajbalani4 <neerajbalani4@gmail.com>
2025-11-27 16:54:20 +0530

    Add Sidebar component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,3 +32,15 @@ export default function RootLayout({
     </html>
   );
 }
+import Sidebar from "@/components/sidebar";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="flex">
+        <Sidebar />
+        <main className="flex-1">{children}</main>
+      </body>
+    </html>
+  );
+}
